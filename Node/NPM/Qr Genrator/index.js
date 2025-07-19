05225dd1c3348ea62e26fe2cae014f01437bc966
Shobhit902 <shobhit1948@gmail.com>
2025-07-19 23:43:23 +0530

    qr genrator

--- Node/NPM/Qr Genrator/index.js
+++ Node/NPM/Qr Genrator/index.js
@@ -1,11 +1,25 @@
 import inquirer from "inquirer";
+import fs from "fs";
 
+import qr from "qr-image";
 inquirer
   .prompt([
     /* Pass your questions in here */
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qrimg.png"));
+
+    fs.writeFile("url.txt", url, (err) => {
+      if (err) throw err;
+      console.log("File saved");
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -14,3 +28,5 @@ inquirer
       // Something else went wrong
     }
   });
+
+var qr_svg = qr.image("I love QR!", { type: "svg" });
