4e5884a934a0709f8eb8674f8441af8b73da8111
Shobhit902 <shobhit1948@gmail.com>
2025-07-20 10:36:34 +0530

    Start with express

---
+++ Express/ 3.1 express/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+    
+})
+
+/*
+app.listen(3000, () => {
+    console.log("Server is running");
+    
+})
+    */
