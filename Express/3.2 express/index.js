7af918c1803797eaae4b41fdff5c1a1998808375
Shobhit902 <shobhit1948@gmail.com>
2025-07-20 17:20:56 +0530

    express server

---
+++ Express/3.2 express/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+// app.get("/", (req, res) => {
+
+//   res.send("Helllo world!")
+//   res.send("<h1>Hello</h1>")
+// });
+
+app.listen(port, () => {
+  console.log(`Server has started at port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Welcome to the homePage</h1>
+         <p>This is the home page of the port ${port}</p>
+         <p>Server is restarting </p>
+         `);
+});
+
+app.get("/about", (req, res) => {
+  res.send(`<p>My self invi the great hahahaha</p>`);
+});
+
+app.get("/contact", (req, res) => {
+  res.send(`<p>phone number 7983243891</p>`);
+});
